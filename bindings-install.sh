daa3be31c89d3cd3082a129e934fe42b9f346570
Tony Tam <tony@eatbacon.org>
2018-12-14 15:08:05 -0800

    fixed path

--- bindings-install.sh
+++ bindings-install.sh
@@ -1,2 +1,5 @@
-mvn install:install-file -Dfile=./fdb-java-6.0.15.jar -DgroupId=com.apple \
-    -DartifactId=fdb-java -Dversion=6.0.15 -Dpackaging=jar
+mvn install:install-file -Dfile=./modules/rebar/fdb-java-6.0.15.jar \
+    -DgroupId=com.apple \
+    -DartifactId=fdb-java \
+    -Dversion=6.0.15 \
+    -Dpackaging=jar
